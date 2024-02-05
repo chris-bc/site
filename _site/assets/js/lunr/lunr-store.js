5a797861b030ce2d2a856dc24fa5c1f2bfc1e8c6
Chris Bennetts-Cash <chris@bennettscash.id.au>
2024-02-05 16:24:46 +1100

    Add generated site for ease of use

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,103 @@
+var store = [{
+        "title": "Cycling from Canberra to Melbourne",
+        "excerpt":"D-Day minus 5 weeks According to my employer I have too much leave. I’ve been directed to take four weeks off before the beginning of June, and as a result will be taking all of May off. What on earth to do with all this time off? Caddy has an...","categories": ["posts"],
+        "tags": ["cycling","endurance","test"],
+        "url": "/posts/cycling-canberra-melbourne/",
+        "teaser": null
+      },{
+        "title": "Australian Alpine Walking Track (the Top Bit)",
+        "excerpt":"At 6pm on December 20, 2013 a ute pulled over at Dead Horse Gap, in the Southern part of Kosciuszko National Park and just South of Thredbo. Three Canberrans got out of the ute and walked to the start of the bush. A few minutes later a sole person returned...","categories": ["posts"],
+        "tags": ["hiking"],
+        "url": "/posts/australian-alpine-walking-track-the-top-bit/",
+        "teaser": null
+      },{
+        "title": "Alpine Expedition Course",
+        "excerpt":"I have been one of the not-quite-fifty NSW SES members accredited in Alpine Search and Rescue, and in early 2013 began looking for ways to improve my skills and increase the capability of the programme beyond what it currently held. With other operators having previously travelled to Canada to undertake...","categories": ["posts"],
+        "tags": ["climbing","alpinism","nz","rescue","ses"],
+        "url": "/posts/alpine-expedition-course/",
+        "teaser": null
+      },{
+        "title": "NZ Ice and Mixed Festival 2014",
+        "excerpt":"Despite spending a significant portion of my latest trip to NZ in bed sick with, variously, a flue, chest infection and asthma, there was still a good deal of extraordinary scenery and I keep inflicting the photos on people. Some people, strangely, have actually been asking to see the photos....","categories": ["posts"],
+        "tags": ["climbing","alpinism","nz"],
+        "url": "/posts/nz-ice-and-mixed-festival-2014/",
+        "teaser": null
+      },{
+        "title": "Aspiring National Park",
+        "excerpt":"In August 2014, shortly after my sister returned from working and trekking in India and Nepal, we caught up at a family party and, along with Wendy, hatched a plan to (optimistically) Climb Mt. Aspiring at the beginning of Summer. Over the next few months plans became more concrete, Wendy...","categories": ["posts"],
+        "tags": ["climbing","alpinism","nz"],
+        "url": "/posts/aspiring-national-park/",
+        "teaser": null
+      },{
+        "title": "The Grand Traverse of the Remarkables Ranges",
+        "excerpt":"The climb was planned for New Year’s Day 2015 and so, naturally, we were looking for an early night to get plenty of rest in for an early start and a big day. We hadn’t quite realised what a madhouse Queenstown became at New Year’s Eve and were pretty worried...","categories": ["posts"],
+        "tags": ["climbing","rock","alpinism","nz"],
+        "url": "/posts/remarkables-grand-traverse/",
+        "teaser": null
+      },{
+        "title": "Summer 2014 Tramping NZ",
+        "excerpt":"We broke up our climbing trip with a lot of walking. And to be fair, a lot of it was scoping out access to crags, but some of it (like the title shot above - The head of the Tasman Glacier in Aoraki-Mt. Cook National Park) was just very nice...","categories": ["posts"],
+        "tags": ["NZ","tramping","hiking","camping"],
+        "url": "/posts/summer-2014-tramping-nz/",
+        "teaser": null
+      },{
+        "title": "Climbing Point Perpendicular (Gallery)",
+        "excerpt":"Over the ANZAC Day weekend my sisters came to visit from Sydney and the Sunshine Coast, and Brittany (Sydney-based new climber) decided we should go climbing at Point Perpendicar, on the Northern end of Jervis Bay, because she loves it there. Point perp is an area of stunning sea cliffs,...","categories": ["posts"],
+        "tags": ["climbing","photography","lightroom"],
+        "url": "/posts/climbing-point-perpendicular-gallery/",
+        "teaser": null
+      },{
+        "title": "Diving on Gilli Air, Indonesia (Gallery)",
+        "excerpt":"After spending the Easter period climbing in the Grampians Wendy and I spent some time in Indonesia, including six solid days getting our open water SCUBA qualification and doing additional dives. It turned out my recently-purchased waterproof, shockproof camera - bought so that I’d stop leaving my camera behind or...","categories": ["posts"],
+        "tags": ["travel","diving","photography","gallery"],
+        "url": "/posts/diving-on-gilli-air-indonesia-gallery/",
+        "teaser": null
+      },{
+        "title": "Adding Photo Galleries to a Jekyll Site",
+        "excerpt":"As you can probably tell I use the Jekyll static site processing engine as the basis for this site. I switched to Jekyll only a few months ago when, after ignoring my site for some time, I wanted to add some content and discovered that my Drupal database had been...","categories": ["posts"],
+        "tags": ["development","web","html","css","javascript"],
+        "url": "/posts/adding-photo-galleries-to-a-jekyll-site/",
+        "teaser": null
+      },{
+        "title": "The New Android Emulator You Have to Try",
+        "excerpt":"Anyone who has ever developed Android applications will agree that the single most frustrating aspect of Android development is the painstaking poor performance and bugginess of Google’s official Android emulator. Networking and file storage don’t work as they should (I once spent about four hours debugging an issue with file...","categories": ["posts"],
+        "tags": ["development","android","mobile"],
+        "url": "/posts/the-new-android-emulator-you-have-to-try/",
+        "teaser": null
+      },{
+        "title": "Router Mod With DD-WRT",
+        "excerpt":"I’ve been interested in custom and open-source router firmware since the early noughties, when Linksys and NetComm began using GPL components and subsequently were required to publish their source code, and did so in an incredibly-difficult-to-compile manner. On and off over the past couple of years I’ve looked at DD-WRT...","categories": ["posts"],
+        "tags": ["Development","Infrastructure","VPN"],
+        "url": "/posts/router-mod-with-dd-wrt/",
+        "teaser": null
+      },{
+        "title": "New Digital Stereo Source",
+        "excerpt":"My 10 year-old Airport Express kicked the bucket last week. I’ve been hoping this day would come for about a year, so I’ve now replaced one of the digital arms of my stereo system. For those that don’t know, my stereo is my pride and joy. It consumes near-endless amounts...","categories": ["posts"],
+        "tags": ["Electronics","Stereo"],
+        "url": "/posts/new-digital-stereo-source/",
+        "teaser": null
+      },{
+        "title": "New Zealand Mountains Summer 2015",
+        "excerpt":"An extraordinarily-busy November included two weddings, working in three different cities and various climbing and canyoning trips. In the midst of this Wendy and I managed to pack and somewhat prepare for a trip into Aoraki/Mt. Cook National Park. With a few peaks in our sights we’d like three good-weather...","categories": ["posts"],
+        "tags": ["climbing","alpinism","nz"],
+        "url": "/posts/new-zealand-summer-2015/",
+        "teaser": null
+      },{
+        "title": "Free Time in NZ (Summer 2015)",
+        "excerpt":"When we last met we were taking a helicopter out of Plateau Hut, returning to Unwin Lodge about a week ahead of schedule. Unfortunately we didn’t take any photos of the wineries (of which there were many), but this is an oveview of some of the other things we filled...","categories": ["posts"],
+        "tags": ["climbing","alpinism","rock","wine","nz"],
+        "url": "/posts/free-time-in-nz-summer-2015/",
+        "teaser": null
+      },{
+        "title": "For the Love of the Munter Hitch",
+        "excerpt":"It’s a clear Spring afternoon. Warmer than you expected, especially after the terrifying move you just pulled off. But you managed to stick it, and with clammy hands that are a little bit shaky you reach the top of pitch 4, build a quick anchor and shout “In hard!”. Leaving...","categories": ["posts"],
+        "tags": ["Climbing","Rescue"],
+        "url": "/posts/some-love-for-the-munter-hitch/",
+        "teaser": null
+      },{
+        "title": "Load Testing the Euro Death Knot (flat overhand)",
+        "excerpt":"As climbers, we all know that the flat overhand knot, or Euro Death Knot (EDK), is preferred over the double fisherman’s bend when joining ropes for a long rappel because its flat profile makes it much less likely to get snagged when pulling the rope. We also know that we...","categories": ["posts"],
+        "tags": ["Climbing","Abseiling"],
+        "url": "/posts/load-testing-the-euro-death-knot/",
+        "teaser": null
+      }]
